9e60b0ef5dafa09a38b76e7cafc32a6105a0fb6b
Inie <me@email.com>
2018-03-15 10:38:35 -0500

    added wikimedia data

---
+++ project/image.py
@@ -0,0 +1,22 @@
+import urllib.parse
+import requests
+import json
+
+people_api = ('https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=?')
+
+# Enter the name of the artist
+user_input = input('name ')
+
+url = people_api + urllib.parse.urlencode({'name': name})
+# print(url)
+json_data = requests.get(url).json()
+
+print(json_data)
+# prinDict = dict(map(int, x.split(':')) for x in json_data)
+# for key in json_data:
+#     print(key)
+
+# with open('people_data', 'w') as f:
+#     data = j
+
+# Extract elements from the response.

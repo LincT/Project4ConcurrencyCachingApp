e5833edc0fcd4ab2f7e308fa14ea4b6961bab9f6
Tyler Caldwell <Talon8989@gmail.com>
2018-03-20 06:21:59 -0500

    Display added

--- project/image.py
+++ project/image.py
@@ -3,13 +3,14 @@ from PIL import Image
 import requests
 import json
 from pprint import pprint
+import webbrowser
 
 # Make an api request
 people_api = 'https://en.wikipedia.org/w/api.php?action=query&format=json&'
 
-user_input = input('name: ')
+artist = input('artist name: ')
 # name = input('name: ')
-url = people_api + urllib.parse.urlencode({'titles': user_input, 'prop': 'images'})
+url = people_api + urllib.parse.urlencode({'titles': artist, 'prop': 'images'})
 # print(url)
 json_data = requests.get(url).json()
 # pprint(json_data)
@@ -31,6 +32,13 @@ for file in files:
 file_list = list(map(lambda each:each.strip("File:"), file_list))
 
 # Request the image url
+new = 2
 new_url = people_api + urllib.parse.urlencode({'titles': "Image:" + file_list[5], 'prop': 'imageinfo', 'iiprop': 'url'})
 image_json_data = requests.get(new_url).json()
-pprint(image_json_data)
+
+# image_key = next(iter(image_json_data['query']['pages'].keys()['imageinfo']['url']))
+image_key = next(iter(image_json_data['query']['pages'].keys()))
+url_image = image_json_data['query']['pages'][image_key]['imageinfo'][0]['url']
+
+webbrowser.open(url_image,new=new)
+# pprint(image_json_data)

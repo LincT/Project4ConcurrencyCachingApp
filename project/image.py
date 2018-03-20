8c57efbdca643540aa9dea5deef707eeec6ac798
Inie <me@email.com>
2018-03-20 09:30:32 -0500

    added some error handdling

--- project/image.py
+++ project/image.py
@@ -9,6 +9,8 @@ import webbrowser
 people_api = 'https://en.wikipedia.org/w/api.php?action=query&format=json&'
 
 artist = input('artist name: ')
+artist = artist.title()
+
 # name = input('name: ')
 url = people_api + urllib.parse.urlencode({'titles': artist, 'prop': 'images'})
 # print(url)
@@ -16,29 +18,34 @@ json_data = requests.get(url).json()
 # pprint(json_data)
 
 # Extract pageid from request response.
-id = next(iter(json_data['query']['pages'].keys()))
+try:
+    id = next(iter(json_data['query']['pages'].keys()))
 
-# Use the pageid to get the pages and get image files
-files = json_data['query']['pages'][id]['images']
-# pprint(file_list)
+    # Use the pageid to get the pages and get image files
+    files = json_data['query']['pages'][id]['images']
+    # pprint(file_list)
 
-# Get image files and put them in a list
-file_list = []
-for file in files:
-    file_list.append(file['title'])
+    # Get image files and put them in a list
+    file_list = []
+    for file in files:
+        file_list.append(file['title'])
 
 
-# Trim the 'File:' part of the image tilte
-file_list = list(map(lambda each:each.strip("File:"), file_list))
+    # Trim the 'File:' part of the image tilte
+    file_list = list(map(lambda each:each.strip("File:"), file_list))
 
-# Request the image url
-new = 2
-new_url = people_api + urllib.parse.urlencode({'titles': "Image:" + file_list[5], 'prop': 'imageinfo', 'iiprop': 'url'})
-image_json_data = requests.get(new_url).json()
+    # Request the image url
+    new = 2
+    for n in range(len(file_list)):
 
-# image_key = next(iter(image_json_data['query']['pages'].keys()['imageinfo']['url']))
-image_key = next(iter(image_json_data['query']['pages'].keys()))
-url_image = image_json_data['query']['pages'][image_key]['imageinfo'][0]['url']
+        new_url = people_api + urllib.parse.urlencode({'titles': "Image:" + file_list[n], 'prop': 'imageinfo', 'iiprop': 'url'})
+        image_json_data = requests.get(new_url).json()
 
-webbrowser.open(url_image,new=new)
+    # image_key = next(iter(image_json_data['query']['pages'].keys()['imageinfo']['url']))
+    image_key = next(iter(image_json_data['query']['pages'].keys()))
+    url_image = image_json_data['query']['pages'][image_key]['imageinfo'][0]['url']
+
+    webbrowser.open(url_image,new=new)
 # pprint(image_json_data)
+except Exception as e:
+    print('Error fetching image because', e)

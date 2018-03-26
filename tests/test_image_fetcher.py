b26756d772971651db8233b46523a450b0609557
Inie <me@email.com>
2018-03-26 16:58:13 -0500

    added a test_image_fetcher

--- tests/test_image_fetcher.py
+++ tests/test_image_fetcher.py
@@ -3,15 +3,12 @@ from project.image_fetcher import image_fetcher
 
 
 class TestImage_Fetcher(TestCase):
-
-
-    def test_image_fetcher(self):
-        artist = Metallica
-        self.assertEqual(Metallica, image_fetcher(artist))
-        
-
-
-
+    # Test the api call output.
+    def test_fetch_output(self):
+        artist = 'michael jackson'
+        image_url = image_fetcher.fetch(self, artist)
+        expected = 'https://upload.wikimedia.org/wikipedia/commons/3/32/1993_walk_of_fame_michael_jackson.jpg'
+        self.assertEqual(expected, image_url)
 
 if __name__ == '__main__':
     main()
